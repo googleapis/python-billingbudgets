628663fe5e51b5c57c461c8594706b441d5658bb
yoshi-automation <yoshi-automation@google.com>
2020-05-29 06:32:46 -0700

    Upgrade protoc-docs-plugin version to 0.7.0 to fix unstable docstrings.
    PiperOrigin-RevId: 312689208
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu May 21 10:00:47 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: dec3204175104cef49bf21d685d5517caaf0058f
    Source-Link: https://github.com/googleapis/googleapis/commit/dec3204175104cef49bf21d685d5517caaf0058f

--- google/cloud/billing_budgets_v1beta1/proto/budget_service_pb2.py
+++ google/cloud/billing_budgets_v1beta1/proto/budget_service_pb2.py
@@ -397,8 +397,6 @@ CreateBudgetRequest = _reflection.GeneratedProtocolMessageType(
         "DESCRIPTOR": _CREATEBUDGETREQUEST,
         "__module__": "google.cloud.billing_budgets_v1beta1.proto.budget_service_pb2",
         "__doc__": """Request for CreateBudget
-  
-  
   Attributes:
       parent:
           Required. The name of the billing account to create the budget
@@ -419,8 +417,6 @@ UpdateBudgetRequest = _reflection.GeneratedProtocolMessageType(
         "DESCRIPTOR": _UPDATEBUDGETREQUEST,
         "__module__": "google.cloud.billing_budgets_v1beta1.proto.budget_service_pb2",
         "__doc__": """Request for UpdateBudget
-  
-  
   Attributes:
       budget:
           Required. The updated budget object. The budget to update is
@@ -446,8 +442,6 @@ GetBudgetRequest = _reflection.GeneratedProtocolMessageType(
         "DESCRIPTOR": _GETBUDGETREQUEST,
         "__module__": "google.cloud.billing_budgets_v1beta1.proto.budget_service_pb2",
         "__doc__": """Request for GetBudget
-  
-  
   Attributes:
       name:
           Required. Name of budget to get. Values are of the form
@@ -465,8 +459,6 @@ ListBudgetsRequest = _reflection.GeneratedProtocolMessageType(
         "DESCRIPTOR": _LISTBUDGETSREQUEST,
         "__module__": "google.cloud.billing_budgets_v1beta1.proto.budget_service_pb2",
         "__doc__": """Request for ListBudgets
-  
-  
   Attributes:
       parent:
           Required. Name of billing account to list budgets under.
@@ -492,8 +484,6 @@ ListBudgetsResponse = _reflection.GeneratedProtocolMessageType(
         "DESCRIPTOR": _LISTBUDGETSRESPONSE,
         "__module__": "google.cloud.billing_budgets_v1beta1.proto.budget_service_pb2",
         "__doc__": """Response for ListBudgets
-  
-  
   Attributes:
       budgets:
           List of the budgets owned by the requested billing account.
@@ -514,8 +504,6 @@ DeleteBudgetRequest = _reflection.GeneratedProtocolMessageType(
         "DESCRIPTOR": _DELETEBUDGETREQUEST,
         "__module__": "google.cloud.billing_budgets_v1beta1.proto.budget_service_pb2",
         "__doc__": """Request for DeleteBudget
-  
-  
   Attributes:
       name:
           Required. Name of the budget to delete. Values are of the form
