814a3a09558afc43cf5b711e26e8b31004735f56
yoshi-automation <yoshi-automation@google.com>
2020-05-29 06:32:46 -0700

    Cloud Billing Budget API v1beta1 Add support for subaccounts and labels filter fields.
    PiperOrigin-RevId: 312682133
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu May 21 09:18:48 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 1b101f55aa42665b71684097256d2a64ff07c670
    Source-Link: https://github.com/googleapis/googleapis/commit/1b101f55aa42665b71684097256d2a64ff07c670

--- google/cloud/billing_budgets_v1beta1/proto/budget_model_pb2.py
+++ google/cloud/billing_budgets_v1beta1/proto/budget_model_pb2.py
@@ -14,6 +14,7 @@ _sym_db = _symbol_database.Default()
 
 from google.api import field_behavior_pb2 as google_dot_api_dot_field__behavior__pb2
 from google.api import resource_pb2 as google_dot_api_dot_resource__pb2
+from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 from google.type import money_pb2 as google_dot_type_dot_money__pb2
 
 
@@ -22,10 +23,11 @@ DESCRIPTOR = _descriptor.FileDescriptor(
     package="google.cloud.billing.budgets.v1beta1",
     syntax="proto3",
     serialized_options=b"\n(com.google.cloud.billing.budgets.v1beta1P\001ZKgoogle.golang.org/genproto/googleapis/cloud/billing/budgets/v1beta1;budgets",
-    serialized_pb=b'\n=google/cloud/billing_budgets_v1beta1/proto/budget_model.proto\x12$google.cloud.billing.budgets.v1beta1\x1a\x1fgoogle/api/field_behavior.proto\x1a\x19google/api/resource.proto\x1a\x17google/type/money.proto"\xde\x03\n\x06\x42udget\x12\x11\n\x04name\x18\x01 \x01(\tB\x03\xe0\x41\x03\x12\x14\n\x0c\x64isplay_name\x18\x02 \x01(\t\x12H\n\rbudget_filter\x18\x03 \x01(\x0b\x32,.google.cloud.billing.budgets.v1beta1.FilterB\x03\xe0\x41\x01\x12G\n\x06\x61mount\x18\x04 \x01(\x0b\x32\x32.google.cloud.billing.budgets.v1beta1.BudgetAmountB\x03\xe0\x41\x02\x12Q\n\x0fthreshold_rules\x18\x05 \x03(\x0b\x32\x33.google.cloud.billing.budgets.v1beta1.ThresholdRuleB\x03\xe0\x41\x01\x12S\n\x10\x61ll_updates_rule\x18\x06 \x01(\x0b\x32\x34.google.cloud.billing.budgets.v1beta1.AllUpdatesRuleB\x03\xe0\x41\x01\x12\x11\n\x04\x65tag\x18\x07 \x01(\tB\x03\xe0\x41\x01:]\xea\x41Z\n$billingbudgets.googleapis.com/Budget\x12\x32\x62illingAccounts/{billing_account}/budgets/{budget}"\xa5\x01\n\x0c\x42udgetAmount\x12.\n\x10specified_amount\x18\x01 \x01(\x0b\x32\x12.google.type.MoneyH\x00\x12T\n\x12last_period_amount\x18\x02 \x01(\x0b\x32\x36.google.cloud.billing.budgets.v1beta1.LastPeriodAmountH\x00\x42\x0f\n\rbudget_amount"\x12\n\x10LastPeriodAmount"\xcd\x01\n\rThresholdRule\x12\x1e\n\x11threshold_percent\x18\x01 \x01(\x01\x42\x03\xe0\x41\x02\x12S\n\x0bspend_basis\x18\x02 \x01(\x0e\x32\x39.google.cloud.billing.budgets.v1beta1.ThresholdRule.BasisB\x03\xe0\x41\x01"G\n\x05\x42\x61sis\x12\x15\n\x11\x42\x41SIS_UNSPECIFIED\x10\x00\x12\x11\n\rCURRENT_SPEND\x10\x01\x12\x14\n\x10\x46ORECASTED_SPEND\x10\x02"H\n\x0e\x41llUpdatesRule\x12\x19\n\x0cpubsub_topic\x18\x01 \x01(\tB\x03\xe0\x41\x02\x12\x1b\n\x0eschema_version\x18\x02 \x01(\tB\x03\xe0\x41\x02"\x90\x02\n\x06\x46ilter\x12\x15\n\x08projects\x18\x01 \x03(\tB\x03\xe0\x41\x01\x12\x66\n\x16\x63redit_types_treatment\x18\x04 \x01(\x0e\x32\x41.google.cloud.billing.budgets.v1beta1.Filter.CreditTypesTreatmentB\x03\xe0\x41\x01\x12\x15\n\x08services\x18\x03 \x03(\tB\x03\xe0\x41\x01"p\n\x14\x43reditTypesTreatment\x12&\n"CREDIT_TYPES_TREATMENT_UNSPECIFIED\x10\x00\x12\x17\n\x13INCLUDE_ALL_CREDITS\x10\x01\x12\x17\n\x13\x45XCLUDE_ALL_CREDITS\x10\x02\x42y\n(com.google.cloud.billing.budgets.v1beta1P\x01ZKgoogle.golang.org/genproto/googleapis/cloud/billing/budgets/v1beta1;budgetsb\x06proto3',
+    serialized_pb=b'\n=google/cloud/billing_budgets_v1beta1/proto/budget_model.proto\x12$google.cloud.billing.budgets.v1beta1\x1a\x1fgoogle/api/field_behavior.proto\x1a\x19google/api/resource.proto\x1a\x1cgoogle/protobuf/struct.proto\x1a\x17google/type/money.proto"\xde\x03\n\x06\x42udget\x12\x11\n\x04name\x18\x01 \x01(\tB\x03\xe0\x41\x03\x12\x14\n\x0c\x64isplay_name\x18\x02 \x01(\t\x12H\n\rbudget_filter\x18\x03 \x01(\x0b\x32,.google.cloud.billing.budgets.v1beta1.FilterB\x03\xe0\x41\x01\x12G\n\x06\x61mount\x18\x04 \x01(\x0b\x32\x32.google.cloud.billing.budgets.v1beta1.BudgetAmountB\x03\xe0\x41\x02\x12Q\n\x0fthreshold_rules\x18\x05 \x03(\x0b\x32\x33.google.cloud.billing.budgets.v1beta1.ThresholdRuleB\x03\xe0\x41\x01\x12S\n\x10\x61ll_updates_rule\x18\x06 \x01(\x0b\x32\x34.google.cloud.billing.budgets.v1beta1.AllUpdatesRuleB\x03\xe0\x41\x01\x12\x11\n\x04\x65tag\x18\x07 \x01(\tB\x03\xe0\x41\x01:]\xea\x41Z\n$billingbudgets.googleapis.com/Budget\x12\x32\x62illingAccounts/{billing_account}/budgets/{budget}"\xa5\x01\n\x0c\x42udgetAmount\x12.\n\x10specified_amount\x18\x01 \x01(\x0b\x32\x12.google.type.MoneyH\x00\x12T\n\x12last_period_amount\x18\x02 \x01(\x0b\x32\x36.google.cloud.billing.budgets.v1beta1.LastPeriodAmountH\x00\x42\x0f\n\rbudget_amount"\x12\n\x10LastPeriodAmount"\xcd\x01\n\rThresholdRule\x12\x1e\n\x11threshold_percent\x18\x01 \x01(\x01\x42\x03\xe0\x41\x02\x12S\n\x0bspend_basis\x18\x02 \x01(\x0e\x32\x39.google.cloud.billing.budgets.v1beta1.ThresholdRule.BasisB\x03\xe0\x41\x01"G\n\x05\x42\x61sis\x12\x15\n\x11\x42\x41SIS_UNSPECIFIED\x10\x00\x12\x11\n\rCURRENT_SPEND\x10\x01\x12\x14\n\x10\x46ORECASTED_SPEND\x10\x02"H\n\x0e\x41llUpdatesRule\x12\x19\n\x0cpubsub_topic\x18\x01 \x01(\tB\x03\xe0\x41\x02\x12\x1b\n\x0eschema_version\x18\x02 \x01(\tB\x03\xe0\x41\x02"\xc4\x03\n\x06\x46ilter\x12\x15\n\x08projects\x18\x01 \x03(\tB\x03\xe0\x41\x01\x12\x66\n\x16\x63redit_types_treatment\x18\x04 \x01(\x0e\x32\x41.google.cloud.billing.budgets.v1beta1.Filter.CreditTypesTreatmentB\x03\xe0\x41\x01\x12\x15\n\x08services\x18\x03 \x03(\tB\x03\xe0\x41\x01\x12\x18\n\x0bsubaccounts\x18\x05 \x03(\tB\x03\xe0\x41\x01\x12M\n\x06labels\x18\x06 \x03(\x0b\x32\x38.google.cloud.billing.budgets.v1beta1.Filter.LabelsEntryB\x03\xe0\x41\x01\x1aI\n\x0bLabelsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12)\n\x05value\x18\x02 \x01(\x0b\x32\x1a.google.protobuf.ListValue:\x02\x38\x01"p\n\x14\x43reditTypesTreatment\x12&\n"CREDIT_TYPES_TREATMENT_UNSPECIFIED\x10\x00\x12\x17\n\x13INCLUDE_ALL_CREDITS\x10\x01\x12\x17\n\x13\x45XCLUDE_ALL_CREDITS\x10\x02\x42y\n(com.google.cloud.billing.budgets.v1beta1P\x01ZKgoogle.golang.org/genproto/googleapis/cloud/billing/budgets/v1beta1;budgetsb\x06proto3',
     dependencies=[
         google_dot_api_dot_field__behavior__pb2.DESCRIPTOR,
         google_dot_api_dot_resource__pb2.DESCRIPTOR,
+        google_dot_protobuf_dot_struct__pb2.DESCRIPTOR,
         google_dot_type_dot_money__pb2.DESCRIPTOR,
     ],
 )
@@ -57,8 +59,8 @@ _THRESHOLDRULE_BASIS = _descriptor.EnumDescriptor(
     ],
     containing_type=None,
     serialized_options=None,
-    serialized_start=992,
-    serialized_end=1063,
+    serialized_start=1022,
+    serialized_end=1093,
 )
 _sym_db.RegisterEnumDescriptor(_THRESHOLDRULE_BASIS)
 
@@ -92,8 +94,8 @@ _FILTER_CREDITTYPESTREATMENT = _descriptor.EnumDescriptor(
     ],
     containing_type=None,
     serialized_options=None,
-    serialized_start=1300,
-    serialized_end=1412,
+    serialized_start=1510,
+    serialized_end=1622,
 )
 _sym_db.RegisterEnumDescriptor(_FILTER_CREDITTYPESTREATMENT)
 
@@ -240,8 +242,8 @@ _BUDGET = _descriptor.Descriptor(
     syntax="proto3",
     extension_ranges=[],
     oneofs=[],
-    serialized_start=189,
-    serialized_end=667,
+    serialized_start=219,
+    serialized_end=697,
 )
 
 
@@ -305,8 +307,8 @@ _BUDGETAMOUNT = _descriptor.Descriptor(
             fields=[],
         )
     ],
-    serialized_start=670,
-    serialized_end=835,
+    serialized_start=700,
+    serialized_end=865,
 )
 
 
@@ -325,8 +327,8 @@ _LASTPERIODAMOUNT = _descriptor.Descriptor(
     syntax="proto3",
     extension_ranges=[],
     oneofs=[],
-    serialized_start=837,
-    serialized_end=855,
+    serialized_start=867,
+    serialized_end=885,
 )
 
 
@@ -382,8 +384,8 @@ _THRESHOLDRULE = _descriptor.Descriptor(
     syntax="proto3",
     extension_ranges=[],
     oneofs=[],
-    serialized_start=858,
-    serialized_end=1063,
+    serialized_start=888,
+    serialized_end=1093,
 )
 
 
@@ -439,10 +441,66 @@ _ALLUPDATESRULE = _descriptor.Descriptor(
     syntax="proto3",
     extension_ranges=[],
     oneofs=[],
-    serialized_start=1065,
-    serialized_end=1137,
+    serialized_start=1095,
+    serialized_end=1167,
 )
 
+
+_FILTER_LABELSENTRY = _descriptor.Descriptor(
+    name="LabelsEntry",
+    full_name="google.cloud.billing.budgets.v1beta1.Filter.LabelsEntry",
+    filename=None,
+    file=DESCRIPTOR,
+    containing_type=None,
+    fields=[
+        _descriptor.FieldDescriptor(
+            name="key",
+            full_name="google.cloud.billing.budgets.v1beta1.Filter.LabelsEntry.key",
+            index=0,
+            number=1,
+            type=9,
+            cpp_type=9,
+            label=1,
+            has_default_value=False,
+            default_value=b"".decode("utf-8"),
+            message_type=None,
+            enum_type=None,
+            containing_type=None,
+            is_extension=False,
+            extension_scope=None,
+            serialized_options=None,
+            file=DESCRIPTOR,
+        ),
+        _descriptor.FieldDescriptor(
+            name="value",
+            full_name="google.cloud.billing.budgets.v1beta1.Filter.LabelsEntry.value",
+            index=1,
+            number=2,
+            type=11,
+            cpp_type=10,
+            label=1,
+            has_default_value=False,
+            default_value=None,
+            message_type=None,
+            enum_type=None,
+            containing_type=None,
+            is_extension=False,
+            extension_scope=None,
+            serialized_options=None,
+            file=DESCRIPTOR,
+        ),
+    ],
+    extensions=[],
+    nested_types=[],
+    enum_types=[],
+    serialized_options=b"8\001",
+    is_extendable=False,
+    syntax="proto3",
+    extension_ranges=[],
+    oneofs=[],
+    serialized_start=1435,
+    serialized_end=1508,
+)
 
 _FILTER = _descriptor.Descriptor(
     name="Filter",
@@ -505,17 +563,53 @@ _FILTER = _descriptor.Descriptor(
             serialized_options=b"\340A\001",
             file=DESCRIPTOR,
         ),
+        _descriptor.FieldDescriptor(
+            name="subaccounts",
+            full_name="google.cloud.billing.budgets.v1beta1.Filter.subaccounts",
+            index=3,
+            number=5,
+            type=9,
+            cpp_type=9,
+            label=3,
+            has_default_value=False,
+            default_value=[],
+            message_type=None,
+            enum_type=None,
+            containing_type=None,
+            is_extension=False,
+            extension_scope=None,
+            serialized_options=b"\340A\001",
+            file=DESCRIPTOR,
+        ),
+        _descriptor.FieldDescriptor(
+            name="labels",
+            full_name="google.cloud.billing.budgets.v1beta1.Filter.labels",
+            index=4,
+            number=6,
+            type=11,
+            cpp_type=10,
+            label=3,
+            has_default_value=False,
+            default_value=[],
+            message_type=None,
+            enum_type=None,
+            containing_type=None,
+            is_extension=False,
+            extension_scope=None,
+            serialized_options=b"\340A\001",
+            file=DESCRIPTOR,
+        ),
     ],
     extensions=[],
-    nested_types=[],
+    nested_types=[_FILTER_LABELSENTRY],
     enum_types=[_FILTER_CREDITTYPESTREATMENT],
     serialized_options=None,
     is_extendable=False,
     syntax="proto3",
     extension_ranges=[],
     oneofs=[],
-    serialized_start=1140,
-    serialized_end=1412,
+    serialized_start=1170,
+    serialized_end=1622,
 )
 
 _BUDGET.fields_by_name["budget_filter"].message_type = _FILTER
@@ -540,9 +634,14 @@ _BUDGETAMOUNT.fields_by_name[
 ].containing_oneof = _BUDGETAMOUNT.oneofs_by_name["budget_amount"]
 _THRESHOLDRULE.fields_by_name["spend_basis"].enum_type = _THRESHOLDRULE_BASIS
 _THRESHOLDRULE_BASIS.containing_type = _THRESHOLDRULE
+_FILTER_LABELSENTRY.fields_by_name[
+    "value"
+].message_type = google_dot_protobuf_dot_struct__pb2._LISTVALUE
+_FILTER_LABELSENTRY.containing_type = _FILTER
 _FILTER.fields_by_name[
     "credit_types_treatment"
 ].enum_type = _FILTER_CREDITTYPESTREATMENT
+_FILTER.fields_by_name["labels"].message_type = _FILTER_LABELSENTRY
 _FILTER_CREDITTYPESTREATMENT.containing_type = _FILTER
 DESCRIPTOR.message_types_by_name["Budget"] = _BUDGET
 DESCRIPTOR.message_types_by_name["BudgetAmount"] = _BUDGETAMOUNT
@@ -707,6 +806,15 @@ Filter = _reflection.GeneratedProtocolMessageType(
     "Filter",
     (_message.Message,),
     {
+        "LabelsEntry": _reflection.GeneratedProtocolMessageType(
+            "LabelsEntry",
+            (_message.Message,),
+            {
+                "DESCRIPTOR": _FILTER_LABELSENTRY,
+                "__module__": "google.cloud.billing_budgets_v1beta1.proto.budget_model_pb2"
+                # @@protoc_insertion_point(class_scope:google.cloud.billing.budgets.v1beta1.Filter.LabelsEntry)
+            },
+        ),
         "DESCRIPTOR": _FILTER,
         "__module__": "google.cloud.billing_budgets_v1beta1.proto.budget_model_pb2",
         "__doc__": """A filter for a budget, limiting the scope of the cost to calculate.
@@ -730,11 +838,26 @@ Filter = _reflection.GeneratedProtocolMessageType(
           omitted, the report will include usage for all the services.
           The service names are available through the Catalog API:
           https://cloud.google.com/billing/v1/how-tos/catalog-api.
+      subaccounts:
+          Optional. A set of subaccounts of the form
+          ``billingAccounts/{account_id}``, specifying that usage from
+          only this set of subaccounts should be included in the budget.
+          If a subaccount is set to the name of the master account,
+          usage from the master account will be included. If omitted,
+          the report will include usage from the master account and all
+          subaccounts, if they exist.
+      labels:
+          Optional. A single label and value pair specifying that usage
+          from only this set of labeled resources should be included in
+          the budget. Multiple entries or multiple values per entry are
+          not allowed. If omitted, the report will include all labeled
+          and unlabeled usage.
   """,
         # @@protoc_insertion_point(class_scope:google.cloud.billing.budgets.v1beta1.Filter)
     },
 )
 _sym_db.RegisterMessage(Filter)
+_sym_db.RegisterMessage(Filter.LabelsEntry)
 
 
 DESCRIPTOR._options = None
@@ -749,7 +872,10 @@ _THRESHOLDRULE.fields_by_name["threshold_percent"]._options = None
 _THRESHOLDRULE.fields_by_name["spend_basis"]._options = None
 _ALLUPDATESRULE.fields_by_name["pubsub_topic"]._options = None
 _ALLUPDATESRULE.fields_by_name["schema_version"]._options = None
+_FILTER_LABELSENTRY._options = None
 _FILTER.fields_by_name["projects"]._options = None
 _FILTER.fields_by_name["credit_types_treatment"]._options = None
 _FILTER.fields_by_name["services"]._options = None
+_FILTER.fields_by_name["subaccounts"]._options = None
+_FILTER.fields_by_name["labels"]._options = None
 # @@protoc_insertion_point(module_scope)
